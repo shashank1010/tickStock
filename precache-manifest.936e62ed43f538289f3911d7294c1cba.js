584e84b3af6e4600539e2c452fa772793f0fb84d
Shashank Agarwal <shashank1010@gmail.com>
2019-05-19 00:26:43 +0530

    Updates

---
+++ precache-manifest.936e62ed43f538289f3911d7294c1cba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898cc694a5d7ce1cc48b88c2b794a88b",
+    "url": "/tickStock/index.html"
+  },
+  {
+    "revision": "969dc12cf747bc954489",
+    "url": "/tickStock/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "88de1c3c86e539824e6a",
+    "url": "/tickStock/static/css/main.b8edea29.chunk.css"
+  },
+  {
+    "revision": "969dc12cf747bc954489",
+    "url": "/tickStock/static/js/2.f9dd59e5.chunk.js"
+  },
+  {
+    "revision": "88de1c3c86e539824e6a",
+    "url": "/tickStock/static/js/main.67c5c3d8.chunk.js"
+  },
+  {
+    "revision": "ba09d3ded5865bc4f70c",
+    "url": "/tickStock/static/js/runtime~main.f8de160a.js"
+  }
+]);
